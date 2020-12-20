f6913227ee8920af1e764a5766ffb0ab84616dbc
MatNaw <mat.nawrot@gmail.com>
2020-12-20 19:14:34 +0100

    Improvement for main * running project should be a bit easier and more clear

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+import argparse
+import os
+
+from constants import MODEL_PATH
 from train import train_network
 from evaluate import predict_images
-import tensorflow as tf
 
 if __name__ == '__main__':
     # tf.enable_eager_execution()
-    #train_network()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", type=int, default=0, choices=[0, 1],
+                        help="Specify if the new model should be trained and saved as './model.h5' (0 - no, 1 - yes")
+    args = parser.parse_args()
+
+    if not os.path.exists(MODEL_PATH) or args.train == 1:
+        train_network()
     predict_images()
