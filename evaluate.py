ae269ad147af1f8a919cca7171069c9abda9d9e9
Bartlomiej <jankowski.bar@gmail.com>
2021-01-27 12:06:09 +0100

    small changes to loading real labels and cleaning code

--- evaluate.py
+++ evaluate.py
@@ -6,13 +6,17 @@ from useful_functions import cut_on_edges, NMS
 from constants import TO_PREDICT_PATH, PREDICTED_PATH, UNIFORM_IMG_SIZE, FIRST_ANCHOR_X, FIRST_ANCHOR_Y, \
     ANCHOR_STEP_X, ANCHOR_STEP_Y, BOX_SIZES, BOX_SCALES, KERAS_IMG_SIZE, CLASS_MODEL_PATH, REGRESSOR_MODEL_PATH, \
     LABEL_PATH
-from train import build_models
+from train import build_models, get_labels
 import cv2
 
 
 def get_label_anchor_box(image_name):
-    padnads_csv_file = pd.read_csv(LABEL_PATH)
-    return padnads_csv_file.loc[padnads_csv_file['image_names'] == image_name][['xmin', 'xmax', 'ymin', 'ymax']].to_numpy()[0]
+    labels = []
+    list_labels = get_labels()
+    for label in list_labels:
+        if label[0] == image_name:
+            labels.append([int(label[2]), int(label[3]), int(label[4]), int(label[5])])
+    return labels
 
 
 def predict_images():
@@ -75,9 +79,6 @@ def predict_images():
                                     box_w = box_x_max - box_x_min 
                                     box_h = box_y_max - box_y_min
 
-                                    # [reg_box_x_min, reg_box_x_max, reg_box_y_min, reg_box_y_max] = \
-                                    #     regressor_pred[k] * UNIFORM_IMG_SIZE[0]
-
                                     [reg_box_x_min, reg_box_x_max, reg_box_y_min, reg_box_y_max] = \
                                         regressor_pred[k]
                                     reg_box_x_min *= box_w
@@ -99,25 +100,16 @@ def predict_images():
                             iterator = 0
                             print("Remaining anchors to iterate: ", remaining_anchors)
 
-        # keras_input=[]
-        # for box in Drones:
-        #      keras_input.append(cv2.resize(sample_image_resized[int(box[2]):int(box[3]), int(box[0]):int(box[1]), 0:3],
-        #                                               KERAS_IMG_SIZE,
-        #                                               interpolation=cv2.INTER_CUBIC))
-        # classifier_pred = classifier.predict_on_batch(np.array(keras_input))
 
         Drones = NMS(Drones, Drones_marks, IoU_threshold=0.1)
 
         # draw the predicted rectangles
-        k=0
         for drone in Drones:
-            # if np.argmax(pred[k]) == 1:
-            #     if pred[k][1] >= 0.90:
             drone = [int(drone[0] * x_ratio), int(drone[1] * x_ratio), int(drone[2] * y_ratio), int(drone[3] * y_ratio)]
             cv2.rectangle(sample_image1, (drone[0], drone[2]), (drone[1], drone[3]), (255, 0, 0), 2)
-            labeled_anchor_box = get_label_anchor_box(image)
+        labeled_anchor_boxes = get_label_anchor_box(image)
+        for labeled_anchor_box in labeled_anchor_boxes:
             cv2.rectangle(sample_image1, (labeled_anchor_box[0], labeled_anchor_box[2]), (labeled_anchor_box[1], labeled_anchor_box[3]), (0, 255, 0), 2)
-            # k=k+1
         cv2.imwrite(PREDICTED_PATH + '/' + image, sample_image1)
         print("Length of Drones: " + str(len(Drones)))
         print("Next image...")
